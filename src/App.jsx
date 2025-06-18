f48a77b1a864d85023b29be0493992af2b1ebfae
DipalokSen <aksharsen68@gmail.com>
2025-06-18 12:22:27 +0600

    Implemented Full Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,56 @@
 
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Components/Search'
+import Spinner from './Components/Spinner';
 
 const App = () => {
- 
- const [searchTerm, setsearchTerm] = useState('');
- 
- const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
- 
-const BASE_URL="https://api.themoviedb.org/3"
 
-const API_OPTIONS={
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
+  const [searchTerm, setsearchTerm] = useState('');
+
+  const [allMovies, setallMovies] = useState([]);
+
+  const [errorMessage, seterrorMessage] = useState(null);
+
+  const [isLoading, setisLoading] = useState(false);
+
+
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+  const BASE_URL = "https://api.themoviedb.org/3"
+
+  const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
   }
-}
- 
-const fetchMovies= async () =>{
+
+  const fetchMovies = async () => {
+    setisLoading(true)
+
+    try {
+      const END_POINT = `${BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(END_POINT, API_OPTIONS);
 
 
-try{
-const END_POIINT=`${BASE_URL}/discover/movie?sort_by=popularity.desc`
-const response=await fetch(END_POIINT, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to Fetch Movies")
+      }
+
+      const data = await response.json()
+
+      if (data.response == false) {
+        seterrorMessage(data.error)
+        setallMovies([])
+
+        return
+      }
+
+      setallMovies(data.results || [])
 
 
-console.log(response);
-
-}
-catch(error){
-  console.error("Error fetching movies:", error);
-}
 
 
 
@@ -40,29 +59,78 @@ catch(error){
 
 
 
-}
 
 
-useEffect(() => {
-  fetchMovies();
-}, []);
- 
+
+    }
+    catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+
+    finally {
+      setisLoading(false)
+    }
+
+
+
+
+
+
+
+  }
+
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
-   <main>
-    <div className='pattern'/>
+    <main>
+      <div className='pattern' />
 
-    <div className="wrapper">
-      <header>
-        <img src="./hero.png" alt="" />
- <h1>Find <span className=' text-gradient'>Movies</span> You will Love Without the hassle</h1>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="" />
+          <h1>Find <span className=' text-gradient'>Movies</span> You will Love Without the hassle</h1>
+          <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+        </header>
 
-      </header>
-      <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm}/>
+        <section className='all-movies'>
+          <h4 className='text-white'>Find All Movies</h4>
 
-     
-    </div>
-    
-   </main>
+          {
+            isLoading ? (
+              <Spinner/>
+            ) : errorMessage ? (
+              <p className='text-white'>Internal Server Error</p>
+            ) : (
+
+
+              <ul>
+
+                {
+
+                  allMovies.map((movie) => (
+                    <li key={movie.id} className='text-white'>{movie.title}</li>
+                  ))}
+
+
+
+
+
+              </ul>
+
+
+
+
+            )
+          }
+        </section>
+
+
+      </div>
+
+    </main>
   )
 }
 
